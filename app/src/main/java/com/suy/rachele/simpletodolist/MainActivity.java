19cc49adddf433a7b9bfa4fafb9de092704cf648
Ermano N. Joseph <jnermano86@gmail.com>
2018-06-30 22:34:49 -0400

    mise a jour du README et ajout de la video

--- app/src/main/java/com/suy/rachele/simpletodolist/MainActivity.java
+++ app/src/main/java/com/suy/rachele/simpletodolist/MainActivity.java
@@ -1,8 +1,7 @@
 package com.suy.rachele.simpletodolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateArrayItems();
-        listViewItem =(ListView) findViewById(R.id.listViewItem);
+        listViewItem = (ListView) findViewById(R.id.listViewItem);
         listViewItem.setAdapter(aTodoAdapter);
         etditText = (EditText) findViewById(R.id.editText);
-        listViewItem.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+        listViewItem.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 todoItems.remove(position);
                 aTodoAdapter.notifyDataSetChanged();
                 writeItems();
@@ -40,31 +39,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        listViewItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                etditText.setText(todoItems.get(position));
+                todoItems.remove(position);
+                aTodoAdapter.notifyDataSetChanged();
+            }
+        });
     }
-    public void populateArrayItems(){
+
+    public void populateArrayItems() {
         readItems();
         aTodoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
 
 
     }
-    private void readItems(){
-        File filesdir =getFilesDir();
+
+    private void readItems() {
+        File filesdir = getFilesDir();
         File file = new File(filesdir, "todo.txt");
-        try{
-            todoItems =new ArrayList<String>(FileUtils.readLines(file));
-        }catch (IOException e){
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+            todoItems = new ArrayList<>();
         }
 
     }
 
 
-    private void writeItems(){
-        File filesdir =getFilesDir();
+    private void writeItems() {
+        File filesdir = getFilesDir();
         File file = new File(filesdir, "todo.txt");
-        try{
-          FileUtils.writeLines(file, todoItems);
-        }catch (IOException e){
-
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
